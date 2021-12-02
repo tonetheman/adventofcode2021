f10072396c0fecb163a862161f562576171dc0ee
tony.chromebook <tonetheman@gmail.com>
2021-12-02 07:25:56 -0600

    both solutions day2

---
+++ day2/Part1.java
@@ -0,0 +1,35 @@
+
+import java.io.*;
+
+public class Part1 {
+
+    public static void main(String[] args) throws Exception {
+        String filename = args[args.length-1];
+
+        int x = 0, y = 0;
+
+        BufferedReader inf = new BufferedReader(new FileReader(filename));
+		String line = null;
+		while (true) {
+			line = inf.readLine();
+			if (line==null) {
+				break;
+			}
+            String []data = line.split(" ");
+            String cmd = data[0];
+			int val = Integer.parseInt(data[1]);
+
+            System.out.println(cmd + " " + val);
+            if ("forward".equals(cmd)) {
+                x += val;
+            } else if ("up".equals(cmd)) {
+                y -= val;
+            } else if ("down".equals(cmd)) {
+                y += val;
+            }
+        }
+        System.out.println(x + " " + y);
+        System.out.println(x*y);
+    }
+
+}
